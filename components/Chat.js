d253c13bc1ca33383b10f984ace4f14c0ac889d5
Giuseppe Mancini <giuseppe.mancini80@gmail.com>
2025-04-22 11:07:04 +0200

    chat.js edited

--- components/Chat.js
+++ components/Chat.js
@@ -1,17 +1,13 @@
 import { useState, useEffect } from "react";
-import { StyleSheet, View } from "react-native";
-import { GiftedChat } from "react-native-gifted-chat";
+import { StyleSheet, View, Platform, KeyboardAvoidingView } from "react-native";
+import { Bubble, GiftedChat } from "react-native-gifted-chat";
 
 const Chat = ({ route, navigation }) => {
-  const { name } = route.params;
   const [messages, setMessages] = useState([]);
-  const onSend = (newMessages) => {
-    setMessages((previousMessages) =>
-      GiftedChat.append(previousMessages, newMessages)
-    );
-  };
+  const { name } = route.params;
 
   useEffect(() => {
+    navigation.setOptions({ title: name });
     setMessages([
       {
         _id: 1,
@@ -23,21 +19,52 @@ const Chat = ({ route, navigation }) => {
           avatar: "https://placeimg.com/140/140/any",
         },
       },
+      {
+        _id: 2,
+        text: "This is a system message",
+        createdAt: new Date(),
+        system: true,
+      },
     ]);
   }, []);
 
-  useEffect(() => {
-    navigation.setOptions({ title: name });
-  }, []);
+  const onSend = (newMessages) => {
+    setMessages((previousMessages) =>
+      GiftedChat.append(previousMessages, newMessages)
+    );
+  };
+
+  const renderBubble = (props) => {
+    return (
+      <Bubble
+        {...props}
+        wrapperStyle={{
+          right: {
+            backgroundColor: "#000",
+          },
+          left: {
+            backgroundColor: "#FFF",
+          },
+        }}
+      />
+    );
+  };
 
   return (
-    <GiftedChat
-      messages={messages}
-      onSend={(messages) => onSend(messages)}
-      user={{
-        _id: 1,
-      }}
-    />
+    <View style={styles.container}>
+      <GiftedChat
+        messages={messages}
+        renderBubble={renderBubble}
+        onSend={(messages) => onSend(messages)}
+        user={{
+          _id: 1,
+          name,
+        }}
+      />
+      {Platform.OS === "android" ? (
+        <KeyboardAvoidingView behavior="height" />
+      ) : null}
+    </View>
   );
 };
 
